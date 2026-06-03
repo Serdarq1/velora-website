b8762334d1e12fd2d482c6c84192ff199eb7c556
Serdar Ziya Akova <serdar.akova5@gmail.com>
2026-06-03 21:14:02 +0300

    add google tag manager

--- velora-landing-page/app/layout.tsx
+++ velora-landing-page/app/layout.tsx
@@ -112,6 +112,17 @@ export default function RootLayout({
     <html lang="tr">
       <head>
         <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-THTDPBTH');`,
+          }}
+        />
+        <Script
           id="schema-organization"
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
@@ -125,6 +136,14 @@ export default function RootLayout({
       <body
         className={`${plusJakartaSans.className} ${inter.className} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-THTDPBTH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <Analytics />
       </body>
